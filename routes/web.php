238b1f35974741d227acef7761f58908b6ab56f6
mustapha jamil <mustaphajay2@gmail.com>
2024-09-21 18:04:59 +0100

    used controllers and a components folder for structure

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,9 @@
 <?php
 
+use App\Http\Controllers\welcomeController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [welcomeController::class, 'index']);
 Route::get('/home', function () {
     return view('home');
 });
